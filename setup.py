f6a12c167232a22df79af5eb16163f293d649321
nilesh-30 <nileshk7017@gmail.com>
2023-12-07 20:19:03 +0530

    cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is to practice git and dvc.',
+    author='Nilesh Kumar',
+    license='',
+)
